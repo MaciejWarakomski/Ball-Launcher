c24626e8a6d36485be5f9ea70843a60f4018fa7a
Warakom <warakom247365@gmail.com>
2021-11-14 17:49:01 +0100

    Read touch screen values

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return;
+        }
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Debug.Log(touchPosition);
+    }
+}
